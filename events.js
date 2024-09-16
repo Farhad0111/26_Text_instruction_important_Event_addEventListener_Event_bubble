22bd8f0b9ae7852bda1c70c4672e285e251a3a78
Farhad0111 <109299643+Farhad0111@users.noreply.github.com>
2024-09-16 13:35:47 +0600

    Add files via upload

--- events.js
+++ events.js
@@ -18,3 +18,22 @@ const purpleButton=document.getElementById('make-purple');
 purpleButton.onclick=function makePurple(){
     document.body.style.backgroundColor='purple';
 }
+
+//option 5
+const pinkButton=document.getElementById('make-pink');
+pinkButton.addEventListener('click',makePink);
+
+function makePink(){
+    document.body.style.backgroundColor='pink';
+}
+
+//option 6
+const makeGreenButton=document.getElementById('make-green');
+makeGreenButton.addEventListener('click',function makeGreen(){
+    document.body.style.backgroundColor='green';
+})
+
+//option 7
+document.getElementById('make-goldenrod').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'goldenrod';
+});
